ffda72e45ce9045e3053d848720e9240a60f975e
SeptAlfauzan <alfauzansepta@gmail.com>
2022-10-26 12:02:12 +0700

    add player moving relative to camera and rotate relative to camera

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -9,8 +9,25 @@ public int movementSpeed;
     // Update is called once per frame
     void Update()
     {
-        Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
-        movementDirection.Normalize();
-        this.transform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
+        MovingRelativeToCamera();
+        transform.rotation = Quaternion.Euler(0, Camera.main.transform.eulerAngles.y, 0);
+        // Vector3 movementDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        // movementDirection.Normalize();
+        // this.transform.Translate(movementDirection * movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    void MovingRelativeToCamera(){
+        float playerVerInput = Input.GetAxis("Vertical");
+        float playerHorInput = Input.GetAxis("Horizontal");
+
+        Vector3 forward = Camera.main.transform.forward;
+        Vector3 right = Camera.main.transform.right;
+
+        Vector3 forwardRelativeVerticalInput = forward * playerVerInput;
+        Vector3 rightRelativeVerticalInput = right * playerHorInput;
+
+        Vector3 cameraRelativeMovement = forwardRelativeVerticalInput + rightRelativeVerticalInput;
+
+        transform.Translate(cameraRelativeMovement * movementSpeed * Time.deltaTime, Space.World);
     }
 }
